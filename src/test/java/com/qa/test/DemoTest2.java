76cd9ca385c03a883eb71fc156adaaa038f34992
shwethaa123 <shwethaa123@yahoo.com>
2018-06-15 12:02:10 +0530

    first maven project

---
+++ src/test/java/com/qa/test/DemoTest2.java
@@ -0,0 +1,45 @@
+package com.qa.test;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class DemoTest2 {
+	@Test
+	public void sum()
+	{
+		System.out.println("SUM");
+		 System.out.println("====================");
+		int a=10;
+		int b=20;
+	    Assert.assertEquals(30, a+b);
+	}
+	@Test
+	public void sub()
+	{
+		System.out.println("SUB");
+		int a=10;
+		int b=20;
+	    Assert.assertEquals(10, b-a);
+	}
+	@Test
+	public void mul()
+	{
+		System.out.println("MUL");
+		int a=10;
+		int b=20;
+	    Assert.assertEquals(200, a*b);
+	}
+	@Test
+	public void div()
+	{
+		System.out.println("DIV");
+	
+		int a=10;
+		int b=20;
+	    Assert.assertEquals(2, b/a);
+	   
+	}
+	
+
+}
